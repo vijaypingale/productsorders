b0f4690f7d98dd78f6d9e4205aba143f546bef92
vijaypingale <vijay.pingale@gmail.com>
2024-05-23 18:10:35 -0400

    Adding missing getters and setters.

--- src/main/java/com/megamart/productsorders/entity/Order.java
+++ src/main/java/com/megamart/productsorders/entity/Order.java
@@ -21,4 +21,44 @@ public class Order {
     private List<Product> products;
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
 }
