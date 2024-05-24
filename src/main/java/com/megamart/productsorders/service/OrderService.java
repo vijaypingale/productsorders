410e37a1fc9bacd5ed8b47bf2e9e1194f5b04e2f
vijaypingale <vijay.pingale@gmail.com>
2024-05-24 14:08:05 -0400

    Adding missing getters and setters.

--- src/main/java/com/megamart/productsorders/service/OrderService.java
+++ src/main/java/com/megamart/productsorders/service/OrderService.java
@@ -1,17 +1,26 @@
 package com.megamart.productsorders.service;
 
 import com.megamart.productsorders.entity.Order;
+import com.megamart.productsorders.entity.Product;
 import com.megamart.productsorders.repository.OrderRepository;
+import com.megamart.productsorders.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Qualifier("orderService")
 public class OrderService {
 
-    private final OrderRepository orderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
@@ -26,6 +35,19 @@ public class OrderService {
     }
 
     public Order createOrder(Order order) {
+        List<Product> managedProducts = new ArrayList<>();
+        for (Product product : order.getProducts()) {
+            if (product.getId() != null) {
+                System.out.println("Adding product:= "+ product.getName());
+                Product managedProduct = productRepository.findById(product.getId())
+                        .orElseThrow(() -> new EntityNotFoundException("Product not found with id " + product.getId()));
+                managedProducts.add(managedProduct);
+            }
+            else {
+                managedProducts.add(product);
+            }
+        }
+        order.setProducts(managedProducts);
         return orderRepository.save(order);
     }
 
